ce87bec63ab0dca021b92e7b9530d45293d7390f
Vidya Ramakrishnan <vidya@hasgeek.in>
2017-10-13 15:33:10 +0530

    Fix travis tests.

--- instance/testing.py
+++ instance/testing.py
@@ -1,36 +1,10 @@
-# -*- coding: utf-8 -*-
-#: Cache type
-CACHE_TYPE = 'redis'
-#: Google Analytics code
-GA_CODE = ''
-#: Google site verification code (inserted as a meta tag)
-GOOGLE_SITE_VERIFICATION = ''
-#: Typekit code
-TYPEKIT_CODE = ''
-#: Database backend
-SQLALCHEMY_DATABASE_URI = 'sqlite://'
-#: Secret key
-SECRET_KEY = 'make this something random'
-#: Lastuser server
-LASTUSER_SERVER = ''
-#: Lastuser client id
+SECRET_KEY = 'testkey'
+SQLALCHEMY_DATABASE_URI = 'postgres://127.0.0.1/dukaan_testing'
+SERVER_NAME = 'dukaan.travis.dev:3000'
+STATIC_SUBDOMAIN = 'static'
+LASTUSER_SERVER = 'https://auth.hasgeek.com'
 LASTUSER_CLIENT_ID = ''
-#: Lastuser client secret
 LASTUSER_CLIENT_SECRET = ''
-#: Mail settings
-#: MAIL_FAIL_SILENTLY : default True
-#: MAIL_SERVER : default 'localhost'
-#: MAIL_PORT : default 25
-#: MAIL_USE_TLS : default False
-#: MAIL_USE_SSL : default False
-#: MAIL_USERNAME : default None
-#: MAIL_PASSWORD : default None
-#: DEFAULT_MAIL_SENDER : default None
-MAIL_FAIL_SILENTLY = False
-MAIL_SERVER = 'localhost'
-DEFAULT_MAIL_SENDER = ('HasGeek', 'test@example.com')
-MAIL_DEFAULT_SENDER = DEFAULT_MAIL_SENDER
-#: Logging: recipients of error emails
-ADMINS = []
-#: Log file
-LOGFILE = 'error.log'
+
+UPLOAD_FOLDER = '/tmp'
+TIMEZONE = 'Asia/Calcutta'
